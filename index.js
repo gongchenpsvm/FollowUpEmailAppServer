c63ccab60205a4ba468d66fb470675650be76c70
gongchen <gongchen@usc.edu>
2017-08-25 12:50:22 -0700

    emable cookies

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//initiate application here
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');//Have access to cookies
+const passport = require('passport');//Tell passport how to use them
 const keys = require('./config/keys');
 require('./models/User');//Order matters
 require('./services/passport');//Use the model
@@ -12,6 +15,18 @@ var promise = mongoose.connect(keys.mongoURI, {
 const authRoutes = require('./routes/authRoutes');
 const app = express();
 authRoutes(app);
+
+//Tell express to use cookie
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,//How long expires in millisecond
+    keys: [keys.cookieKey]
+  })
+);
+//Tell passport to use cookie
+app.use(passport.initialize());
+app.use(passport.session());
+
 // app.get('/', (req, res) => {
 //   res.send({ bye : 'buddy' });
 // });
